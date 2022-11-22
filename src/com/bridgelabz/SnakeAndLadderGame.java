994c53ed47c82e02e449a66655cb3d54b1723ecb
Govardhan5612 <govardhansiripireddy5612@gmail.com>
2022-11-22 15:17:11 +0530

    Ensure the player gets to exact winning position at 100

--- src/com/bridgelabz/SnakeAndLadderGame.java
+++ src/com/bridgelabz/SnakeAndLadderGame.java
@@ -11,15 +11,16 @@ public class SnakeAndLadderGame {
 
             int randomDice = (int) ((Math.random() * 10) % 6 + 1);
 
-
+                //snake
             if (sumOfRollDie==20 || sumOfRollDie==35 || sumOfRollDie==50 || sumOfRollDie==65 || sumOfRollDie==80 ){
                 sumOfRollDie-=15;
             }
+            //ladder
             if (sumOfRollDie==15 || sumOfRollDie==30 || sumOfRollDie==45 || sumOfRollDie==60 || sumOfRollDie==75 ){
                 sumOfRollDie+=15;
             }
 
-            if (sumOfRollDie >=100){
+            if (sumOfRollDie >=94){
                 break;
             }
             playerPosition++;
@@ -29,12 +30,26 @@ public class SnakeAndLadderGame {
             System.out.println("Player number of roll die - "+playerPosition+" and position - "+sumOfRollDie);
 
         }
+        int cont =0;
+        int dif = 100-sumOfRollDie;
+        while (playerPosition <=100){
+            int randomDice = (int) ((Math.random() * 10) % 6 + 1);
+            if (cont == dif){
+                break;
+            }
+            cont++;
+            playerPosition++;
+        }
+        int totalSumOfRollDies = cont+sumOfRollDie;
+        System.out.println("Player number of roll die - "+playerPosition+" and position - "+totalSumOfRollDies);
+
+
         return playerPosition;
 
     }
 
     public static void main(String[] args) {
         int playerOne = playerOne();
-        System.out.println("Player won at the position : "+playerOne);
+        System.out.println("Player won at the number of roll dies : "+playerOne);
     }
 }
